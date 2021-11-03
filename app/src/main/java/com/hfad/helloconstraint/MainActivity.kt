81db430a939279f5e43eed197b0e1e9ca92276e1
Ekaterina Pavlova <ekaterinapavlova_96@mail.ru>
2021-11-03 23:17:47 +0300

    Used ViewBinding

--- app/src/main/java/com/hfad/helloconstraint/MainActivity.kt
+++ app/src/main/java/com/hfad/helloconstraint/MainActivity.kt
@@ -3,17 +3,14 @@ package com.hfad.helloconstraint
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
 import helloconstraint.R
+import helloconstraint.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private var mCount = 0
-    private lateinit var mShowCount: TextView
-    private lateinit var buttonZero: Button
-    private lateinit var buttonCount: Button
+    private lateinit var binding: ActivityMainBinding
 
     companion object {
         private const val KEY_COUNT = "KEY_COUNT"
@@ -22,18 +19,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        mShowCount = findViewById(R.id.show_count)
-        buttonZero = findViewById(R.id.button_zero)
-        buttonCount = findViewById(R.id.button_count)
-
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
         checkCount(mCount)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
         mCount = savedInstanceState.getInt(KEY_COUNT)
-        mShowCount.text = mCount.toString()
+        binding.showCount.text = mCount.toString()
         checkCount(mCount)
 
     }
@@ -49,30 +42,30 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun countUp(view: android.view.View) {
-        mCount ++
-        mShowCount.text = mCount.toString()
+        mCount++
+        binding.showCount.text = mCount.toString()
         checkCount(mCount)
     }
 
     fun returnZero(view: android.view.View) {
         mCount = 0
-        mShowCount.text = mCount.toString()
+        binding.showCount.text = mCount.toString()
         checkCount(mCount)
     }
 
-    private fun checkCount (mCount: Int){
+    private fun checkCount(mCount: Int) {
         when {
             mCount == 0 -> {
-                buttonZero.setBackgroundColor(Color.GRAY)
-                buttonCount.setBackgroundColor(Color.BLUE)
+                binding.buttonCount.setBackgroundColor(Color.BLUE)
+                binding.buttonZero.setBackgroundColor(Color.GRAY)
             }
-            mCount %2 == 0 -> {
-                buttonCount.setBackgroundColor(Color.BLUE)
-                buttonZero.setBackgroundColor(Color.MAGENTA)
+            mCount % 2 == 0 -> {
+                binding.buttonCount.setBackgroundColor(Color.BLUE)
+                binding.buttonZero.setBackgroundColor(Color.MAGENTA)
             }
             else -> {
-                buttonCount.setBackgroundColor(Color.GREEN)
-                buttonZero.setBackgroundColor(Color.MAGENTA)
+                binding.buttonCount.setBackgroundColor(Color.GREEN)
+                binding.buttonZero.setBackgroundColor(Color.MAGENTA)
             }
         }
     }
